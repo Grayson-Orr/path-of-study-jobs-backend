c844512936401a5db86f0c33de64ae8f191a0fe1
Grayson Orr <GraysonO@op.ac.nz>
2023-05-22 19:27:33 +1200

    style: format code

--- web-scraper/job/job/spiders/job.py
+++ web-scraper/job/job/spiders/job.py
@@ -1,6 +1,6 @@
 import scrapy
 from job.items import JobItem
-import datetime
+
 
 class JobSpider(scrapy.Spider):
     name = "job"
@@ -12,10 +12,10 @@ class JobSpider(scrapy.Spider):
     def parse(self, response):
         links = response.xpath(
             '//a[@data-automation="jobTitle"]/@href').getall()
-        
-        for l in links:
-            jobLink = response.urljoin(l)
-            yield scrapy.Request(jobLink, callback=self.parse_item)
+
+        for link in links:
+            job_link = response.urljoin(link)
+            yield scrapy.Request(job_link, callback=self.parse_item)
 
         next_page = response.xpath('//a[@title="Next"]/@href').extract_first()
         url = response.urljoin(next_page)
@@ -23,20 +23,14 @@ class JobSpider(scrapy.Spider):
 
     def parse_item(self, response):
         item = JobItem()
-        
         item['job'] = response.xpath(
             '//h1[@data-automation="job-detail-title"]/text()').extract()
-
         item['company'] = response.xpath(
             '//span[@data-automation="advertiser-name"]/text()').extract()
-
         item['location'] = response.xpath(
             '//div[@class="_1wkzzau0 a1msqi5a a1msqiga"]//div//div//span/text()').extract_first()
-
         item['type'] = response.xpath(
             '//span[@data-automation="job-detail-work-type"]/text()').extract()
-
         item['description'] = response.xpath(
             '//div[@data-automation="jobAdDetails"]/div//text()').extract()
-        
         yield item
